3f0edac25ce6b497312b5363aad8404c96adf649
masche-mat <86981242+masche-mat@users.noreply.github.com>
2021-08-14 14:11:39 -0300

    Update login.js
    funcion para redirigir la pagina

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
+function enviar(){
+    window.location.replace('https://masche-mat.github.io/workspace-inicial/home.html');
+}    
+
+
+
+
+
+
+
+
+/*//Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
-});
+});*/
