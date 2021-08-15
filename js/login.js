e45b9eb5e3919cc63abd5a3cb0812c82b19b134b
masche-mat <86981242+masche-mat@users.noreply.github.com>
2021-08-14 21:25:23 -0300

    Update login.js
    agrego funcion de redireccion al ingresar

--- js/login.js
+++ js/login.js
@@ -1,17 +1,14 @@
-function enviar(){
-    window.location.replace('https://masche-mat.github.io/workspace-inicial/home.html');
-}    
+var formulario = document.getElementById('formIngreso');
 
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
 
-
-
-
-
+    window.location.replace('https://masche-mat.github.io/workspace-inicial/home.html')
+});
 
 
 /*//Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});*/
+document.addEventListener("DOMContentLoaded", function(e){}
+*/
